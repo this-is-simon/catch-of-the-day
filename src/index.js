101b6d4676f276c1349cc1b831e58d58bb14f416
Simon Atkins <simonatk@gmail.com>
2018-12-18 11:06:03 +0000

    Add goToStore function to allow users to enter website upon clicking button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Router from './components/Router';
-import App from './components/App';
-import NotFound from './components/NotFound';
 import './css/style.css';
 
 
