179b31f287250a4f33042f01d2e17ef358ff278c
Simon Atkins <simonatk@gmail.com>
2019-01-14 11:20:45 +0000

    Add header, details and button to Fish components in menu

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
   render() {
+    const { image, name, price, desc, status } = this.props.details
     return(
       <li className='menu-fish'>
-        <img src={this.props.details.image} alt={this.props.details.name} />
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button>Add to Cart</button>
       </li>
     )
   }
