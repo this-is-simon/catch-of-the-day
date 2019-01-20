c1ecba588561f86cb24df3887df885ff1e4f2018
Simon Atkins <simonatk@gmail.com>
2019-01-20 18:33:15 +0000

    Add Firebase app but throwing browser error

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
   state = {
