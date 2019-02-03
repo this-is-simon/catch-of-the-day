69dac3952c55bff5c6553058c7ab3435b77798f3
Simon Atkins <simonatk@gmail.com>
2019-02-03 19:07:00 +0000

    Complete bi-directional data flow so that inventory changes are reflected in the main app

--- src/components/App.js
+++ src/components/App.js
@@ -36,6 +36,12 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  updateFish = (key, updatedFish) => {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   }
@@ -68,6 +74,7 @@ class App extends React.Component {
         />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,16 +1,52 @@
 import React from 'react';
 
 class EditFishForm extends React.Component {
+  handleChange = (event) => {
+    const updatedFish = {
+      ...this.props.fish,
+      [event.currentTarget.name]: event.currentTarget.value
+    };
+    this.props.updateFish(this.props.index, updatedFish)
+  }
+
   render () {
     return <div className='fish-edit'>
-      <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
-      <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
-      <select name="status" ref={this.statusRef}>
-        <option value="available">Fresh!</option>
-        <option value="unavailable">Sold Out!</option>
+      <input
+        name="name"
+        ref={this.nameRef}
+        type="text"
+        value={this.props.fish.name}
+        onChange={this.handleChange}
+      />
+      <input
+        name="price"
+        ref={this.priceRef}
+        type="text"
+        value={this.props.fish.price}
+        onChange={this.handleChange}
+      />
+      <select
+        name="status"
+        ref={this.statusRef}
+        value={this.props.fish.status}
+        onChange={this.handleChange}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
       </select>
-      <textarea name="desc" ref={this.descRef} type="text" placeholder="Desc"></textarea>
-      <input name="img" ref={this.imgRef} type="text" placeholder="Img" />
+      <textarea
+        name="desc"
+        ref={this.descRef}
+        type="text"
+        value={this.props.fish.desc}
+        onChange={this.handleChange}>
+      </textarea>
+      <input
+        name="image"
+        ref={this.imgRef}
+        type="text"
+        value={this.props.fish.image}
+        onChange={this.handleChange}
+      />
     </div>
   }
 }
