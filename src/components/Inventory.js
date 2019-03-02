3e7ccb94b491abd8e066e4df4e06eae69682fcf5
Simon Atkins <simonatk@gmail.com>
2019-03-02 19:15:42 +0000

    Add authentication buttons for Inventory

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
 
 class Inventory extends React.Component {
 
@@ -13,6 +14,7 @@ class Inventory extends React.Component {
   };
 
   render() {
+    return <Login />;
     return(
       <div className='inventory'>
         <h2>Inventory</h2>
---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Login = () => (
+  <nav className='login'>
+  <h2>Inventory Login</h2>
+  <p>Sign in to manage your store's inventory</p>
+  <button
+    className='github'
+    onClick={() => this.props.authenticate('Github')}>
+    Log In With Github
+  </button>
+  <button
+    className='facebook'
+    onClick={() => this.props.authenticate('Facebook')}>
+    Log In With Facebook
+  </button>
+  <button
+    className='twitter'
+    onClick={() => this.props.authenticate('Twitter')}>
+    Log In With Twitter
+  </button>
+  </nav>
+)
+
+export default Login
